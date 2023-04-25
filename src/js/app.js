cbf9e035ace931edf2aaa1f9dfe80041f3553d9f
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2023-04-24 23:46:46 -0400

    Added links for hedermenu and footer; added new images for services

--- src/js/app.js
+++ src/js/app.js
@@ -27,36 +27,14 @@ plus.forEach((element, i) => {
       descr[previouslyClickedIndex].classList.remove('active_flex');
       button[previouslyClickedIndex].classList.remove('active');
       minus[previouslyClickedIndex].classList.remove('active_plus');
-
-      switch (true) {
-        case (previouslyClickedIndex < 7):
-          image[0].classList.remove('active');
-          break;
-        case (previouslyClickedIndex < 9):
-          image[1].classList.remove('active');
-          break;
-        default:
-          image[2].classList.remove('active');
-          break;
-      }
+		image[previouslyClickedIndex].classList.remove('active');
     }
 
     // Toggle the active classes and the display of the corresponding content
     descr[i].classList.toggle('active_flex');
     button[i].classList.toggle('active');
     minus[i].classList.toggle('active_plus');
-
-    switch (true) {
-      case (i < 7):
-        image[0].classList.toggle('active');
-        break;
-      case (i < 9):
-        image[1].classList.toggle('active');
-        break;
-      default:
-        image[2].classList.toggle('active');
-        break;
-    } 
+	 image[i].classList.toggle('active');
 
     previouslyClickedIndex = i;
   });
